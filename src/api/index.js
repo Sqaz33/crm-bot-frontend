f8a3db858734565ad673c1bb490cf3a695a32033
MDA <dimka5rus@yandex.ru>
2025-06-12 04:12:35 +0300

    фикс .env

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useAuthStore } from '../stores/auth'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',  
+  baseURL: import.meta.env.API_URL, 
 })
 
 api.interceptors.request.use((config) => {
