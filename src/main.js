79a49c664b85dde33cf264dc76fcc7b07672a78a
MDA <dimka5rus@yandex.ru>
2025-06-22 00:10:08 +0300

    тест

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 
 import VueDatePicker from '@vuepic/vue-datepicker'
 import '@vuepic/vue-datepicker/dist/main.css'
 
-const app = createApp(App) 
+const app = createApp(App)
 
-app.component('VueDatePicker', VueDatePicker) 
+
+const pinia = createPinia()
+app.use(pinia)
+
+
+app.component('VueDatePicker', VueDatePicker)
+
+
 app.use(router).mount('#app')
