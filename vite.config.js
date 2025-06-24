6f6c89318544a03b3a322df5c740be445e9213e9
MDA <dimka5rus@yandex.ru>
2025-06-24 06:24:26 +0300

    vite config fix

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,11 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: true, 
+    allowedHosts: [
+      'www.crm-bot.dev.groza1338.ru'
+    ],
     proxy: {
-      // Проксируем все запросы к /api/* на реальный API-сервер
       '/api': {
         target: 'https://api.crm-bot.dev.groza1338.ru',
         changeOrigin: true,
