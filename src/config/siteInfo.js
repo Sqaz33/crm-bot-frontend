1380eca7a353342df07c42b5ecbbcf2ff5b95c67
Dmitry Morozov <dimka5rus@yandex.ru>
2025-07-03 14:53:35 +0300

    адрес сайтинфо

--- src/config/siteInfo.js
+++ src/config/siteInfo.js
@@ -1,6 +1,6 @@
 export const siteInfo = {
     bot_name: "Aura_bot",
-    address: "https://clck.ru/3Mv9sM",
+    address: "https://yandex.ru/maps/-/CHsZqZ4b",
     about_company: "Задача нашего сервиса - собрать всю необходимую информацию о салонах красоты в Волгограде для того, чтобы вы могли выбрать наиболее подходящий для вас."
   }
    
