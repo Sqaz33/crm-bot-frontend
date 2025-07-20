4de3054b1c216cf90b9caa5493550a1f3fbc5829
razumovskayama <razmarant@gmail.com>
2025-07-20 18:46:03 +0300

    список сотрудников

--- src/views/ChoiceStaff.vue
+++ src/views/ChoiceStaff.vue
@@ -87,8 +87,7 @@ onMounted(loadStaff)
 }
 
 .filter-title {
-  font-weight: bold;
-  margin: 0.5rem;
+  margin: 1rem;
 }
 
 .staff-list {
@@ -96,12 +95,16 @@ onMounted(loadStaff)
   flex-direction: column;
   gap: 1rem;
 }
+
 .staff-card {
   display: flex;
   align-items: center;
   background: #fff;
   padding: 1rem;
   border-radius: 8px;
+  max-width: 1140px; 
+  margin: 1.5rem;
+  padding: 0.5rem;
 }
 .avatar {
   width: 40px; height: 40px;
