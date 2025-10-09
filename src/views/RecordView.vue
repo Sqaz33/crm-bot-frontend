527233e4c25033524a33390e6674ae7c58360c84
Matveev Stepan Andreevich <mset321@gmail.com>
2025-10-09 18:33:08 +0300

    fix: измененение времени в DOM после переноса записи

--- src/views/RecordView.vue
+++ src/views/RecordView.vue
@@ -264,6 +264,7 @@ async function goToDatetime() {
       visit_date_time: visitDateISO, 
       will_come: visit.value.will_come 
     });
+    visit.visit_date_time = visitTime;
 
   } catch (err) {
     console.error(err);
