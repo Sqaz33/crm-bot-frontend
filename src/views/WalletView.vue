757847a5403f6b48d2eeb6f99b1ec4ed852ee766
luhavadd <luhavadiana5@gmail.com>
2025-10-18 22:14:24 +0300

    кошелек

--- src/views/WalletView.vue
+++ src/views/WalletView.vue
@@ -3,8 +3,8 @@
         
     
         
-    <main class="empty-content">
-      <img src="../assets/SpiderWallet.svg" alt="Пустой кошелёк" class="empty-icon" />
+    <main class="form-container">
+      <img src="../assets/SpiderWallet.svg" alt="Пустой кошелёк" class="nothing-icon" />
       <p1>Увы, в кошельке пусто</p1>
       <p2>У Вас пока нет ни одной карты лояльности</p2>        
     </main>
@@ -22,3 +22,38 @@
 
 
 </script>
+<style>
+.form-container {
+  flex: 1;
+  display: flex;
+  padding:6rem;
+  align-items: center;
+  flex-direction: column;
+  overflow-y: auto;
+  
+  
+}
+
+.form-container p1 {
+  font-size: clamp(0.75rem,4vw,1.2rem);
+}
+
+.form-container p2{
+  font-size: clamp(0.6rem,3vw,1rem);
+}
+
+
+.nothing-icon{
+  width:clamp(6.7rem, 10vw, 8rem);
+  height: clamp(6.7rem, 10vw, 8rem);
+  margin-bottom: 1rem;
+}
+
+@media (max-width: 768px) {
+  .form-container {
+    padding-right: 3rem; 
+  }
+}
+
+
+</style>
