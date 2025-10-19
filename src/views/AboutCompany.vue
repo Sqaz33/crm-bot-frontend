e9650c46fd5e2d2d952e6f80c1feed8c68bc873d
luhavadd <luhavadiana5@gmail.com>
2025-10-19 14:58:24 +0300

    шрифт

--- src/views/AboutCompany.vue
+++ src/views/AboutCompany.vue
@@ -215,10 +215,10 @@ onMounted(async () => {
     padding-right: clamp(0.2rem, 3vw, 4rem);
   }
   .reviewer-info .reviewer-name {
-	font-size: 0.75rem;
+	font-size: 0.85rem;
   }
   .review-text {
-  font-size: 0.7rem;
+  font-size: 0.75rem;
 }
 }
 
