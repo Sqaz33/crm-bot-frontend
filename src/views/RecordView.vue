7621e3d0a3ff7c928af7020a2d92ab1aaabe8e32
luhavadd <luhavadiana5@gmail.com>
2025-10-27 16:55:21 +0300

    ширина

--- src/views/RecordView.vue
+++ src/views/RecordView.vue
@@ -427,7 +427,7 @@ async function submitReview() {
   background: #fff;
   border-radius: 10px;
   box-shadow: 0 2px 4px rgba(0,0,0,0.1);
-  width: 320px;
+  width: 300px;
   padding:clamp(0.6rem, 1.2vw, 1.5rem);
 }
 
