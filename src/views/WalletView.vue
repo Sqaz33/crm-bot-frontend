9f93b6e6176e3435da73291c009ed2efed52b2e8
razumovskayama <razumovskaya.masha@inbox.ru>
2025-11-02 21:05:55 +0300

    кошелек

--- src/views/WalletView.vue
+++ src/views/WalletView.vue
@@ -27,6 +27,11 @@
   flex: 1;
   display: flex;
   padding:0;
+	position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
   align-items: center;
   flex-direction: column;
   overflow-y: auto;
