6d0377bddc3f6cbf0f131b1acc14e64bf8e0241c
Matveev Stepan Andreevich <mset321@gmail.com>
2025-11-05 16:57:52 +0300

    fix: кнопка перехода к созданию записи

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="base">
 
     <!-- Верхняя кнопка -->
-    <button class="main-button" @click="goTo('appointment')">
+    <button class="main-button" @click="goTo('appointmant')">
       Персональные услуги
     </button>
 
