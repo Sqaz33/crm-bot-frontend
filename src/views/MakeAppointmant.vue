690600c45cb91bc12b395ea96311c2a43caa1dda
razumovskayama <razumovskaya.masha@inbox.ru>
2025-11-09 19:25:55 +0300

    еще побольше кнопки

--- src/views/MakeAppointmant.vue
+++ src/views/MakeAppointmant.vue
@@ -422,7 +422,7 @@ function confirmProfile() {
   font-weight: 500;
   cursor: pointer;
   transition: all 0.2s ease;
-  width: clamp(120px, 3vw, 320px);
+  width: clamp(120px, 3vw, 720px);
 }
 
 .modal-buttons button:first-child {
