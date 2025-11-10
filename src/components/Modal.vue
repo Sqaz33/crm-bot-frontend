8e04ce90a1aa3dd846fcbacdd42beb2307ef95d2
MDA <dimka5rus@yandex.ru>
2025-11-10 16:41:55 +0300

    модалку ставою по центру

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -72,7 +72,7 @@ onBeforeUnmount(() => setScrollLock(false))
   align-items: center;
 
 
-  --hpad: max(16px, max(env(safe-area-inset-left), env(safe-area-inset-right)));
+  /* --hpad: max(16px, max(env(safe-area-inset-left), env(safe-area-inset-right))); */
   --vpad: max(16px, max(env(safe-area-inset-top),  env(safe-area-inset-bottom)));
   padding-inline: var(--hpad);
   padding-block: var(--vpad);
