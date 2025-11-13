2a80222326229c8e96673fcc1fb650eb1dcfea80
Matveev Stepan Andreevich <mset321@gmail.com>
2025-11-14 02:02:50 +0300

    fix: daytime x5

--- src/views/DateTime.vue
+++ src/views/DateTime.vue
@@ -261,7 +261,7 @@ export default {
 
       try {
         const { data } = await api.get('/staff/free_time/', { params })
-        const slots = data.available_slots
+        const slots = data[0].available_slots
         this.freeSlots = slots.map(slot => {
           const s = slot.start
           let utcString
