6221ca34adca57cb529dcdefef27983358e1eb8e
MDA <dimka5rus@yandex.ru>
2025-11-14 05:31:41 +0300

    вот ты где сука

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,13 +1,13 @@
 import api from './index'
 
 export function loginViaTelegram(initDataStr) {
-  return api.post('/auth/telegram/login', { init_data: initDataStr })
+  return api.post('/auth/telegram/login/', { init_data: initDataStr })
 }
 
 export function logoutSession() {
-  return api.post('/auth/logout') 
+  return api.post('/auth/logout/') 
 }
 
 export function getMe() {
-  return api.get('/auth/me')       
+  return api.get('/auth/me/')       
 }
