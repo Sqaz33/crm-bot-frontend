d87eaaf9eaac68a2bf60fdb6d7427651444fd84f
MDA <dimka5rus@yandex.ru>
2025-12-05 17:27:19 +0300

    заглушка для пилота

--- src/components/ShareModal.vue
+++ src/components/ShareModal.vue
@@ -54,8 +54,9 @@
   <script setup>
   import { ref } from 'vue'
 
-  const botUsername = '@CheckAuthorization_bot'
-  const botLink = `https://t.me/CheckAuthorization_bot`
+ const botLink = import.meta.env.VITE_BOT_LINK || 'https://t.me/CheckAuthorization_bot'
+ const botUsername = import.meta.env.VITE_BOT_NAME || '@CheckAuthorization_bot'
+
   const shareText = encodeURIComponent(`Попробуй Telegram-бота ${botUsername}`)
   const urlEncoded = encodeURIComponent(botLink)
 
