6e74f8916139603ff72796c2c16df6658718dcc3
Matveev Stepan Andreevich <mset321@gmail.com>
2025-12-06 18:54:18 +0300

    fix: убрал часовой пояс из списка записей

--- src/views/RecordsView.vue
+++ src/views/RecordsView.vue
@@ -228,7 +228,8 @@ function switchTab(tab) {
 }
 
 function formatDateShort(iso) {
-  const d = new Date(iso.endsWith("Z") ? iso : iso + "Z");
+  // const d = new Date(iso.endsWith("Z") ? iso : iso + "Z");
+  const d = new Date(iso)
   const day = String(d.getDate()).padStart(2, "0");
   const month = String(d.getMonth() + 1).padStart(2, "0");
   const year = String(d.getFullYear()).slice(-2);
@@ -236,7 +237,8 @@ function formatDateShort(iso) {
 }
 
 function formatTimeOnly(iso) {
-  const d = new Date(iso.endsWith("Z") ? iso : iso + "Z");
+  // const d = new Date(iso.endsWith("Z") ? iso : iso + "Z");
+  const d = new Date(iso)
   const hours = String(d.getHours()).padStart(2, "0");
   const minutes = String(d.getMinutes()).padStart(2, "0");
   return `${hours}:${minutes}`;
