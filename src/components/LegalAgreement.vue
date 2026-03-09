ed0769d842135f7675093afdf6eb02373f56c9a1
Stepan Matveev <mset321@gmail.com>
2026-03-09 18:10:26 +0300

    fix: скруглил кнопку

--- src/components/LegalAgreement.vue
+++ src/components/LegalAgreement.vue
@@ -1,21 +1,33 @@
 <template>
   <div class="flex items-center gap-2 mb-4 mt-2">
-    <input
-      type="checkbox"
-      :id="checkboxId"
-      :checked="modelValue"
-      @change="$emit('update:modelValue', $event.target.checked)"
-      class="w-6 h-6 rounded-lg border-neutral-300 accent-brand-500 focus:ring-brand-500"
-    />
-    <label :for="checkboxId" class="text-sm text-gray-700">
-      <span>Я принимаю </span>
-      <a 
-        href="#" 
-        @click.prevent="$emit('show-terms')"
-        class="text-[#3471D6] underline"
-      >
-        условия использования
-      </a>
+    <label :for="checkboxId" class="flex items-center gap-2 cursor-pointer">
+      <div class="relative w-6 h-6">
+        <input
+          type="checkbox"
+          :id="checkboxId"
+          :checked="modelValue"
+          @change="$emit('update:modelValue', $event.target.checked)"
+          class="peer appearance-none w-6 h-6 rounded-md border-2 border-neutral-300 bg-white cursor-pointer 
+                 checked:border-brand-500 checked:bg-brand-500 transition-colors"
+        />
+        <svg
+          v-if="modelValue"
+          class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-4 h-4 text-white pointer-events-none"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+        </svg>
+      </div>
+      <span class="text-sm text-gray-700">
+        <span>Я принимаю </span>
+        <span 
+          @click.prevent="$emit('show-terms')"
+          class="text-brand-500 underline"
+        >
+          условия использования
+        </span>
+      </span>
     </label>
   </div>
 </template>
